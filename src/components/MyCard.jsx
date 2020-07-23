71541504c7095bbf4f357fb19e37937b5ccc963c
koob <zeebha@gmail.com>
2020-07-23 17:45:50 +0200

    axios/get profile

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,40 +1,14 @@
 import React from "react";
 
-import { Button } from "react-bootstrap";
+
 
 import "../App.css";
 import "./styles/cards.css";
 
 const MyCard = () => {
 	return (
-		<div className="wrapper flexColumn">
-			
-			<div className="myActions flexrow">
-					<Button variant="send">envoyer</Button>
-					<Button variant="modify">modifer</Button>
-				</div>
-
-			<div className="myCardForm flexrow">
-				<div className='cardTitle flexrow'>
-					<div>Name</div>
-					<div>Diet</div>
-				</div>
-			</div>
-
-
-			<div className="myCardForm">
-					<div className="foodDetails">Food Details</div>
-			</div>
-
-			<div className="myCardForm">				
-					<div className="foodDetails">User Details</div>
-			</div>
-
-			<div className="myCardForm">
-				<div className="myCollection">MyCollection</div>
-			</div>
-
-			</div>
+<>
+</>
 
 	);
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,20 +1,65 @@
 import React, { useState, useEffect }from 'react';
+import { Button } from "react-bootstrap";
 
-import MyCard from './MyCard';
+import "../App.css";
+import "./styles/cards.css";
+import Axios from 'axios';
+
 
 const Profile = () => {
 
-  const [profile, setProfile] = useState();
+  const [profile, setProfile] = useState([]);
 
   const currentUserId = localStorage.getItem("currentUserId")
-  
+
+ const getCurrentUserData = () => {
+  const url = `http://localhost:8000/api/users/${currentUserId}`;
+  Axios.get(url)
+  .then((response) => response.data)
+  .then((data)=> setProfile(data[0]));
+}
+
+  useEffect(() => {
+    getCurrentUserData();
+  }, []);
+
+console.log(profile)
+
+  if(profile){
+    return (
+      <div>
+    		<div className="wrapper flexColumn">
+			
+			<div className="myActions flexrow">
+					<Button variant="send">envoyer</Button>
+					<Button variant="modify">modifer</Button>
+				</div>
+
+			<div className="myCardForm flexrow">
+				<div className='cardTitle flexrow'>
+        <div>{profile.name}</div>
+					<div>{profile.diet}</div>
+				</div>
+			</div>
 
 
-  return (
-    <div>
-    <MyCard />
+			<div className="myCardForm">
+					<div className="foodDetails">Food Details</div>
+			</div>
+
+			<div className="myCardForm">				
+					<div className="foodDetails">User Details</div>
+			</div>
+
+			<div className="myCardForm">
+				<div className="myCollection">MyCollection</div>
+			</div>
+
+			</div>
     </div>
   );
+}
+return null;
 };
 
 export default Profile;
