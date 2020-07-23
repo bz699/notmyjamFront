594a8a9f60a14af19368f7b4b747d7682128f7fc
koob <zeebha@gmail.com>
2020-07-23 19:14:52 +0200

    axios/set diet put

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Col, Form } from "react-bootstrap";
 import Axios from "axios";
 
 import "../App.css";
 import "./styles/cards.css";
 
 const Profile = () => {
-
 	const [profile, setProfile] = useState([]);
 
 	const currentUserId = localStorage.getItem("currentUserId");
@@ -23,19 +22,37 @@ const Profile = () => {
 		getCurrentUserData();
 	}, []);
 
+	// modify Current User datas
 
-  // modify Current User datas
+	const handleTextChange = (event) => {
+		const { name, value } = event.target;
+		setProfile({ ...profile, [name]: value });
+	};
 
-  const handleTextChange = (event) => {
-    const { name, value } = event.target
-    setProfile({...profile, [name]: value})
-  }
-  
 	const updateUser = () => {
 		const url = `http://localhost:8000/api/users/${currentUserId}`;
-    Axios.put(url, profile)
-    .then(alert("Le profil a bien été mis à jour"));
+		Axios.put(url, profile).then(alert("Le profil a bien été mis à jour"));
 	};
+
+  // modify diet name
+	const [diets, setDiets] = useState([]);
+
+	const getDietsData = () => {
+		const url = "http://localhost:8000/api/diets";
+		Axios.get(url)
+			.then((response) => response.data)
+			.then((data) => setDiets(data));
+	};
+
+	useEffect(() => {
+		getDietsData();
+  }, []);
+  
+  const handleChangeDiet = (event) => {
+    const {name, value} = event.target;
+    setProfile({...profile, [name]:value })
+
+  }
 
 	console.log(profile);
 
@@ -43,45 +60,53 @@ const Profile = () => {
 		return (
 			<div>
 				<div className="wrapper flexColumn">
-        <Form
-          onSubmit={() => {
-            updateUser();
-          }}>
-					<div className="myActions flexrow">        
-						<Button variant="send">envoyer</Button>
-						<Button
-              variant="modify"
-              type="submit">modifer</Button>
-					</div>
-          
-          
-              <Form.Group>
-                <Form.Label> </Form.Label>
-                <Form.Control
-                  type="text"
-                  name="name"
-                  value={profile.name}
-                  onChange={handleTextChange}
-                  placeholder={profile.name} />
-              </Form.Group>
-
-					<div className="myCardForm flexrow">
-						<div className="cardTitle flexrow">
-							<div>{profile.name}</div>
-							<div>{profile.diet}</div>
+					<Form
+						onSubmit={() => {
+							updateUser();
+						}}
+					>
+						<div className="myActions flexrow">
+							<Button variant="send">envoyer</Button>
+							<Button variant="modify" type="submit">
+								modifer
+							</Button>
 						</div>
-					</div>
 
-					<div className="myCardForm">
-						<div className="foodDetails">Food Details</div>
-					</div>
+            <div className="myCardForm flexrow">
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Form.Control
+                    type="text"
+                    name="name"
+                    value={profile.name}
+                    onChange={handleTextChange}
+                    placeholder={profile.name}
+                  />
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Form.Control
+                    as="select"
+                    name="diet_id"
+                    value={profile.diet_id}
+                    onChange={handleChangeDiet}
+                  >
+                    <option value="choose">Choisir...</option>
+                    {diets.map((diet) => (
+                      <option value={diet.id}>{diet.name}</option>
+                    ))}
+                  </Form.Control>
+                </Form.Group>
+                </Form.Row>
+						</div>
 
-					<div className="myCardForm">
-						<div className="foodDetails">User Details</div>
-					</div>
-          
+						<div className="myCardForm">
+							<div className="foodDetails">Food Details</div>
+						</div>
 
-          </Form>
+						<div className="myCardForm">
+							<div className="foodDetails">User Details</div>
+						</div>
+					</Form>
 					<div className="myCardForm">
 						<div className="myCollection">MyCollection</div>
 					</div>
