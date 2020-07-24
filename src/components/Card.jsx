fe3540517509ddeec130ccda5c214eb7bafcfb43
koob <zeebha@gmail.com>
2020-07-24 15:25:33 +0200

    css/work on card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,80 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 import "../App.css";
 import "./styles/cards.css";
 
 const Card = (user) => {
+	// GET DIET
+	const [diets, setDiets] = useState([]);
 
+	const getDietsData = () => {
+		const url = "http://localhost:8000/api/diets";
+		Axios.get(url)
+			.then((response) => response.data)
+			.then((data) => setDiets(data));
+	};
 
-    // GET DIET
-    const [ diets, setDiets] = useState([]);
+	useEffect(() => {
+		getDietsData();
+	}, []);
 
-    const getDietsData = () => {
-      const url = "http://localhost:8000/api/diets";
-      Axios.get(url)
-        .then((response) => response.data)
-        .then((data) => setDiets(data));
-    };
-  
-    useEffect(() => {
-      getDietsData();
-    }, []);
-  
-    const getDietName = (dietId) => {
-      const foundDiet = diets.find((diet) => diet.id === dietId);
-      return foundDiet ? foundDiet.name : 'o_o';
-    };
+	const getDietName = (dietId) => {
+		const foundDiet = diets.find((diet) => diet.id === dietId);
+		return foundDiet ? foundDiet.name : "o_o";
+	};
 
-    // GET FOOD
-    // get food lists
-    const [ foodList, setFoodList] = useState([]);
+	// GET FOOD
+	// get food lists
+	const [foodList, setFoodList] = useState([]);
 
-    const getFoodList = () => {
-      const url = `http://localhost:8000/api/users/${user.user.id}/foodList`;
-      Axios.get(url)
-        .then((response) => response.data)
-        .then((data) => setFoodList(data));
-    };
+	const getFoodList = () => {
+		const url = `http://localhost:8000/api/users/${user.user.id}/foodList`;
+		Axios.get(url)
+			.then((response) => response.data)
+			.then((data) => setFoodList(data));
+	};
 
-    useEffect(() => {
-      getFoodList();
-    }, []);
+	useEffect(() => {
+		getFoodList();
+	}, []);
 
-    // get food name
-    const [ foods, setFoods] = useState([]);
+	// get food name
+	const [foods, setFoods] = useState([]);
 
-    const getFoodData = () => {
-      const url = 'http://localhost:8000/api/foods';
-      Axios.get(url)
-        .then((response) => response.data)
-        .then((data) => setFoods(data));
-    };
+	const getFoodData = () => {
+		const url = "http://localhost:8000/api/foods";
+		Axios.get(url)
+			.then((response) => response.data)
+			.then((data) => setFoods(data));
+	};
 
-    useEffect(() => {
-      getFoodData();
-    }, []);
+	useEffect(() => {
+		getFoodData();
+	}, []);
 
-    const getFoodName = (foodId) => {
-      const foundFood = foods.find((food) => food.id === foodId);
-      return foundFood ? foundFood.item : '-_o';
-    };
+	const getFoodName = (foodId) => {
+		const foundFood = foods.find((food) => food.id === foodId);
+		return foundFood ? foundFood.item : "-_o";
+	};
 
-
-    return (
-
-      <div className="cardSmall">
-        <div className='myDetails'>
-            <div className='cardTitle flexColumn'>
-              <div className='userName'>{user.user.name}</div>
-              <div className='userDiet' >{getDietName(user.user.diet_id)}</div>
-            </div>
-  
-          <div className='foodDetails'>{foodList.map((list) => <p>{getFoodName(list.food_id)}</p>)}
+	return (
+		<div className="cardSmall">
+			<div className="myDetails">
+				<div className="cardTitle flexColumn">
+					<div className="userName">
+              <p>{user.user.name}</p>
           </div>
-        </div>
+					<div className="userDiet">{getDietName(user.user.diet_id)}</div>
+				</div>
       </div>
-    );
+
+			<div className="foodDetails">
+        <p className="hot">not my jam !</p>
+					{foodList.map((list) => (
+						<p>{getFoodName(list.food_id)}</p>
+					))}
+			</div>
+		</div>
+	);
 };
 
 export default Card;
